223ab500bcda899035011e989131eb51ccba1fe9
motephyr <motephyr@gmail.com>
2014-01-30 01:42:39 +0800

    setting db & home link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def login_required
+    if current_user.blank?
+      respond_to do |format|
+        format.html {
+          authenticate_user!
+        }
+        format.js {
+          render :partial => "common/not_logined"
+        }
+        format.all {
+          head(:unauthorized)
+        }
+      end
+    end
+  end
 end
