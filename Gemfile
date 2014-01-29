7d5cf401cfa1da2143f8565f71a9b54bcb6045e7
motephyr <motephyr@gmail.com>
2014-01-30 01:57:21 +0800

    push to heroku

--- Gemfile
+++ Gemfile
@@ -44,9 +44,6 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
 
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
-
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,9 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Utool::Application.config.secret_key_base = '90f53893a9f2a2bc69bf333102c938abc1d4c8213b5826962fea2646efde1b878f43ebdbc4c21b19dc3c4c38467c82390057b5b998543d22f09c6a847a53f98c'
+if Rails.env.test? || Rails.env.development? || Rails.env == "profile"
+  Utool::Application.config.secret_token = "90f53893a9f2a2bc69bf333102c938abc1d4c8213b5826962fea2646efde1b878f43ebdbc4c21b19dc3c4c38467c82390057b5b998543d22f09c6a847a53f98c"
+else
+  raise "You must set a secret token in ENV['SECRET_TOKEN'] or in config/initializers/secret_token.rb" if ENV['SECRET_TOKEN'].blank?
+  Utool::Application.config.secret_token = ENV['SECRET_TOKEN']
+end
