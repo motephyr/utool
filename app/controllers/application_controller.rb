01a0df42cad71b758808f751e6495c3686aad140
motephyr <motephyr@gmail.com>
2014-02-02 00:42:42 +0800

    個人資料修改畫面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   protect_from_forgery with: :exception
   def login_required
     if current_user.blank?
@@ -17,4 +19,8 @@ class ApplicationController < ActionController::Base
       end
     end
   end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password,:password_confirmation)} end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @links = params[:category_id] ? current_user.links.where(category_id: params[:category_id]) : current_user.links
-    @categories = current_user.categories
+    @links = params[:category_id] ? @user.links.where(category_id: params[:category_id]) : @user.links
+    @categories = @user.categories
   end
 end
