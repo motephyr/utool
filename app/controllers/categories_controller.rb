485422dd995fc49fba5d6b576a033181ff177cef
motephyr <motephyr@gmail.com>
2014-02-04 00:33:26 +0800

    類別的新增修改刪除頁面

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,37 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.where(user: current_user);
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
   def create
     @category = current_user.categories.build(category_params)
     @category.save
 
-    redirect_to user_path(current_user)
+    redirect_to categories_path
+  end
+
+  def update
+    @category = current_user.categories.find(params[:id])
+    if @category.update(category_params)
+      redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = current_user.categories.find(params[:id])
+    @category.destroy
+
+    redirect_to categories_path
   end
 
   private
