6661d20b11c6146fb2996702d3b327dcb966dd97
motephyr <motephyr@gmail.com>
2014-02-12 00:24:04 +0800

    整理html

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,5 +1,9 @@
 module LinksHelper
-  def set_title_link(title,link)
+  def render_link_title(title, link)
     link.blank? ?  "未設定" : link_to(title, link)
+  end
+
+  def render_link_image(link)
+    image_tag(link.thumbnail_url ? link.thumbnail_url : 'noimage.jpg' , style: "vertical-align:middle;")
   end
 end
