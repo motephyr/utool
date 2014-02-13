6396acf70e95dec03245fd071d7ee484096df314
motephyr <motephyr@gmail.com>
2014-02-14 00:29:29 +0800

    dix notice message

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -6,4 +6,14 @@ module LinksHelper
   def render_link_image(link)
     image_tag(link.thumbnail_url ? link.thumbnail_url : 'noimage.jpg' , style: "vertical-align:middle;")
   end
+
+  def notice_message
+    flash_messages = []
+    flash.each do |type, message|
+      type = :success if type == :notice
+      text = content_tag(:div, link_to("x", "#", :class => "close", "data-dismiss" => "alert", :onclick => "document.getElementById('close').style.display='none'") + message, :id => "close", :class => "alert fade in alert-#{type}")
+      flash_messages << text if message
+    end
+    flash_messages.join("\n").html_safe
+  end
 end
